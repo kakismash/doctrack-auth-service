1d2e17a290ce5e3f53bdd55bf688412c7fb3920a
Joaquin Navarro <alfian1991@gmail.com>
2025-02-26 20:04:07 -0500

    adding doctrack-frontend

--- src/main/java/com/kaki/doctrack/authservice/init/DataInitializer.java
+++ src/main/java/com/kaki/doctrack/authservice/init/DataInitializer.java
@@ -16,46 +16,46 @@ import reactor.core.publisher.Mono;
 @Configuration
 @RequiredArgsConstructor
 public class DataInitializer {
-//
-//    private final UserRepository userRepository;
-//    private final RoleRepository roleRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @Bean
-//    public ApplicationRunner initializer() {
-//        return args -> initRoles()
-//            .then(initSuperAdminUser()).subscribe();
-//    }
-//
-//    private Mono<Void> initRoles() {
-//        return createRoleIfNotExists(ERole.SUPER_ADMIN)
-//                .then(createRoleIfNotExists(ERole.ADMIN))
-//                .then(createRoleIfNotExists(ERole.ORGANIZATION_ADMIN))
-//                .then(createRoleIfNotExists(ERole.ORGANIZATION_WORKER))
-//                .then();
-//    }
-//
-//    private Mono<Void> createRoleIfNotExists(ERole role) {
-//        return roleRepository.findByName(role.name())
-//                .switchIfEmpty(roleRepository.save(new Role(null, role.name())))
-//                .then();
-//    }
-//
-//    private Mono<Void> initSuperAdminUser() {
-//        return roleRepository.findByName(ERole.SUPER_ADMIN.name())
-//                .flatMap(role -> userRepository.findByUsername("kaki1991")
-//                        .switchIfEmpty(Mono.defer(() -> {
-//                            User user = new User();
-//                            user.setUsername("kaki1991");
-//                            user.setRole(role);
-//                            user.setEmail("alfian1991@gmail.com");
-//                            user.setFirstname("Joaquin");
-//                            user.setLastname("Navarro");
-//                            user.setStatus(EUserStatus.ACTIVE.ordinal());
-//                            user.setPhone("0123456789");
-//                            user.setPassword(passwordEncoder.encode("kaki1991"));
-//                            return userRepository.save(user);
-//                        }))
-//                ).then();
-//    }
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public ApplicationRunner initializer() {
+        return args -> initRoles()
+            .then(initSuperAdminUser()).subscribe();
+    }
+
+    private Mono<Void> initRoles() {
+        return createRoleIfNotExists(ERole.SUPER_ADMIN)
+                .then(createRoleIfNotExists(ERole.ADMIN))
+                .then(createRoleIfNotExists(ERole.ORGANIZATION_ADMIN))
+                .then(createRoleIfNotExists(ERole.ORGANIZATION_WORKER))
+                .then();
+    }
+
+    private Mono<Void> createRoleIfNotExists(ERole role) {
+        return roleRepository.findByName(role.name())
+                .switchIfEmpty(roleRepository.save(new Role(null, role.name())))
+                .then();
+    }
+
+    private Mono<Void> initSuperAdminUser() {
+        return roleRepository.findByName(ERole.SUPER_ADMIN.name())
+                .flatMap(role -> userRepository.findByUsername("kaki1991")
+                        .switchIfEmpty(Mono.defer(() -> {
+                            User user = new User();
+                            user.setUsername("kaki1991");
+                            user.setRole(role);
+                            user.setEmail("alfian1991@gmail.com");
+                            user.setFirstname("Joaquin");
+                            user.setLastname("Navarro");
+                            user.setStatus(EUserStatus.ACTIVE.ordinal());
+                            user.setPhone("0123456789");
+                            user.setPassword(passwordEncoder.encode("kaki1991"));
+                            return userRepository.save(user);
+                        }))
+                ).then();
+    }
 }
